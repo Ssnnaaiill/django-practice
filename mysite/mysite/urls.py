7c73d7f26379084e4da6896d4caad6b261fb206e
project <Ssnnaaiill>
2019-05-09 22:06:41 -0700

    190511

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('polls/', include('polls.urls')),
 ]
